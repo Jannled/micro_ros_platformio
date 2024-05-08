e18495f9f3541528fd669d7142accc6a42a206d8
Jannik <Jannled@users.noreply.github.com>
2024-05-08 10:20:07 +0200

    Update utils.py

--- microros_utils/utils.py
+++ microros_utils/utils.py
@@ -2,7 +2,7 @@ import subprocess
 
 def run_cmd(command, env=None):
     return subprocess.run(command,
-        capture_output = True,
+        capture_output = False,
         shell = True,
         env=env
     )
